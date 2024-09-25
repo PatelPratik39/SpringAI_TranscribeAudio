3c5c7e95514077c1c763dea1f8cab0ac7166e339
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-25 13:57:04 -0500

    Creat new App using SpringAI that takes Audio as parameter and convert it into text

---
+++ audio-transcribe/src/main/java/com/audio/audio_transcribe/AudioTranscribeApplication.java
@@ -0,0 +1,13 @@
+package com.audio.audio_transcribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTranscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTranscribeApplication.class, args);
+	}
+
+}
---
+++ audio-transcribe/src/test/java/com/audio/audio_transcribe/AudioTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.audio_transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
